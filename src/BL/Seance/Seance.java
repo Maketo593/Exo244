66008f329cf73bdbabc701a959a2225b504096b0
MatteoCasu <ra-9817@student.iram.be>
2025-10-07 04:16:59 +0200

    Ajout de la classe Seance dans le BL

---
+++ src/BL/Seance/Seance.java
@@ -0,0 +1,36 @@
+package BL.Seance;
+import BL.Cours.Cours;
+import BL.Local.Local;
+import java.sql.Timestamp;
+public class Seance {
+
+    private final int id;
+    private Cours cours;
+    private Local local;
+    private Timestamp date;
+
+    public Seance(int id, Cours cours, Local local, Timestamp date) {
+        this.id = id;
+        this.cours = cours;
+        this.local = local;
+        this.date = date;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public Cours getCours() {
+        return this.cours;
+    }
+
+    public void setCours(Cours cours) {
+        this.cours = cours;
+    }
+
+    public Local getLocal() {
+        return this.local;
+    }
+    public Timestamp getDate() {
+        return this.date;
+    }
+}
